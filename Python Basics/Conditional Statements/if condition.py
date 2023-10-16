c68b7f2c519ef48f3f3767d6a7fffe980676b612
Danvanth Darshan <94788427+dd7430@users.noreply.github.com>
2023-10-16 23:05:44 +0800

    Update if condition.py

--- Python Basics/Conditional Statements/if condition.py
+++ Python Basics/Conditional Statements/if condition.py
@@ -5,3 +5,33 @@ if a>=35:
   print("Pass")
 else:
   print("Fail")
+
+
+#Using elif condition
+a = int(input("Enter your marks: "))
+
+if a>90:
+  print("Topper")
+elif a>60:
+  print("First Class")
+elif a>=35: #multiple elif can be used
+  print("Just Pass")
+else:
+  print("Fail")
+
+#nested if (a if condition inside a if condition)
+
+a = int(input("Enter the marks: "))
+subject = input("Enter the subject: ")
+
+if subject =="Maths":
+  if a>90:
+    print("Topper")
+  elif a>60:
+      print("First Class")
+  elif a>=35: #multiple elif can be used
+      print("Just Pass")
+  else:
+    print("Fail")
+else:
+  print("Not here")
