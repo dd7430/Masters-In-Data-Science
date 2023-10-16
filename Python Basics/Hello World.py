bcfd9a50a01eb079b836df2e6f8f4f470654c016
Danvanth Darshan <94788427+dd7430@users.noreply.github.com>
2023-10-16 15:49:04 +0800

    Create Hello World.py

---
+++ Python Basics/Hello World.py
@@ -0,0 +1,27 @@
+print("Hello World")
+print(5)
+
+#Assigning variables
+a = 4
+b = 5
+c = a+b
+print(c)
+
+#Assigning as Strings
+
+a = "3"
+b = "4"
+c = a + b
+print(c)
+
+#Creating an Input
+
+z = input("Enter a number: ")
+print(z * 5) 
+#Here you can see the output will not be the multiplication of 5 but it prints the same value 5 times because it sees as a string
+
+
+#Using input functions as integers
+
+a =int(input("Enter a number: "))
+print(a * 5)
