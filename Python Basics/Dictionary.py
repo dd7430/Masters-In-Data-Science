e37392b4b9b4d62d53926f5d6466b5ac6c128289
Danvanth Darshan <94788427+dd7430@users.noreply.github.com>
2023-10-16 22:12:11 +0800

    Dictionary.py

---
+++ Python Basics/Dictionary.py
@@ -0,0 +1,17 @@
+# { key : value}
+
+a = {10 : "Sachin", 18 : "Kholi", 1 : "KL Rahul", 45 : "Rohit", 7 : "Dhoni"}
+print(a)
+
+#Update a Dictionary
+
+a = {10 : "Sachin", 18 : "Kholi", 1 : "KL Rahul", 45 : "Rohit", 10 : "Dhoni"}
+
+#To print a particular value in a dictionary keys are used
+
+print(a[45])
+
+#replace the value
+
+a[1] = "Ishant Sharma"
+print(a)
