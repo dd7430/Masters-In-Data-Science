0871f216ac560a5e37a3dbdd56cc3dde4541e99e
Danvanth Darshan <94788427+dd7430@users.noreply.github.com>
2023-10-18 01:42:15 +0800

    Update functions.py

--- Python Basics/functions.py
+++ Python Basics/functions.py
@@ -16,3 +16,21 @@ def cubeNumber(x):
 a = cubeNumber(3)
 
 print(a)
+
+def add_1():
+  a=1
+  b=2
+  c=a+b
+  print(c)
+
+add_1() + 3 # error will show
+
+#returning value
+
+def add_2():
+  a=1
+  b=2
+  c=a+b
+  return c
+
+print(add_2()+3) # program runs without errors
