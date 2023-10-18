8d954b55b17ae4c787f730ed9cac992c2b0b73d0
Danvanth Darshan <94788427+dd7430@users.noreply.github.com>
2023-10-18 22:01:33 +0800

    Update functions.py

--- Python Basics/functions.py
+++ Python Basics/functions.py
@@ -34,3 +34,30 @@ def add_2():
   return c
 
 print(add_2()+3) # program runs without errors
+
+#using def to create variables inside def
+def add_2(a,b):
+  c=a+b
+  print c
+
+add_2(5,10)
+
+
+#local and global variables
+
+a = 10 #global variable
+def cat():
+  c='virat'
+  print c
+
+print c # c will not print because it is a local variable inside the function. Without calling the function you cannot print 'virat'
+
+#giving values for variables inside the def function
+
+def add_2(a=5,b=20):
+  c=a+b
+  print (c)
+
+add_2()
+add_2(12,11) # if values are given to the variables it uses the new values if written by the def function. 
+  
